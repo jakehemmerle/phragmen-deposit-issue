5f3f5ed699c7e049e68e953bcb6cdb08fa3ed54b
a-jwc <chauajw@gmail.com>
2021-12-22 10:19:27 -0800

    wip: get reserve amount at current block

--- finder.js
+++ finder.js
@@ -1,5 +1,7 @@
 // Import the API
 const { ApiPromise, WsProvider } = require("@polkadot/api");
+const csv = require("@fast-csv/format");
+const fs = require("fs");
 
 async function main() {
 	// Initialise the provider to connect to the local node
@@ -7,36 +9,55 @@ async function main() {
 
 	// Create the API and wait until ready
 	const api = await ApiPromise.create({ provider });
-	const specVersion = (await api.query.system.lastRuntimeUpgrade()).toString();
+	// runtime 28 startblock
 	let blockNumber = 3899547;
+
 	// first hit according to https://gist.github.com/emielvanderhoek/0a6cf51393e8d22de364b777f98cd453
 	blockNumber = 3911406;
-	// blockNumber = 4353551;
+	// blockNumber = 4353551; // 200960000000
+	// blockNumber = 4300648; // truncated
+	blockNumber = 4204227;
+
 	const endingBlockNumber = 8591976;
+	const data = {};
+	const rows = [];
+	const stream = csv.format();
+	// stream.pipe(process.stdout);
 
 	while (blockNumber < endingBlockNumber) {
 		let blockHash = await api.rpc.chain.getBlockHash(blockNumber);
 		console.log(`- block number: ${blockNumber}`);
 		let signedBlock = await api.rpc.chain.getBlock(blockHash);
-
+		if ((await api.rpc.state.getRuntimeVersion()).toString() > 31) {
+			break;
+		}
 		signedBlock.block.extrinsics.forEach(async (extrinsic) => {
-			// TODO: Tweak to find call index of elections.vote and elections.removeVoter
-			if (
-				(extrinsic.callIndex[0] == 17 && extrinsic.callIndex[1] == 1) ||
-				(extrinsic.callIndex[0] == 17 && extrinsic.callIndex[1] == 0)
-			) {
+			if (extrinsic.callIndex[0] == 17) {
 				// Get sender address
 				const sender = extrinsic.signer.toString();
-				const reserves = await api.query.balances.reserves(sender);
-				const version = await api.query.balances.storageVersion();
+				data.sender = sender;
+				data.block = blockNumber;
+				// Returns a decorated API instance at the current block hash
+				const apiAt = await api.at(blockHash);
+				const voting = await apiAt.query.electionsPhragmen.voting(sender);
+				data.votingReserve = voting.deposit.toNumber() / 10000000000;
+				const account = await apiAt.query.system.account(sender);
+				data.accountReserves = account.data.reserved.toNumber() / 10000000000;
+				rows.push(data);
+				console.log(rows);
 
-				const voting = await api.query.elections.voting(sender);
-				console.log(voting);
+				if (extrinsic.callIndex[1] == 0) {
+					console.log("vote called");
+					// if(data.votingReserve == )
+				} else if (extrinsic.callIndex[1] == 1) {
+					console.log("remove_voter called");
+				}
 			}
 		});
 		blockNumber++;
 	}
-	// console.log(`runtime version: ${(await api.rpc.state.getRuntimeVersion()).toString()}`);
+	// stream.write([data]);
+	// stream.end();
 }
 
 main().catch((error) => {
