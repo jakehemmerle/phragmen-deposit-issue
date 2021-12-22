9edcf07b0f13a074c83eb67ab5f6cca905f4e165
Jake Hemmerle <jakehemmerle@protonmail.com>
2021-12-22 16:46:41 -0500

    current state

--- finder.js
+++ finder.js
@@ -4,6 +4,8 @@ const csv = require("@fast-csv/format");
 const fs = require("fs");
 
 async function main() {
+
+	const PLANCK_PER_DOT = 10000000000;
 	// Initialise the provider to connect to the local node
 	const provider = new WsProvider("wss://rpc.polkadot.io");
 
@@ -16,11 +18,14 @@ async function main() {
 	blockNumber = 3911406;
 	// blockNumber = 4353551; // 200960000000
 	// blockNumber = 4300648; // truncated
-	blockNumber = 4204227;
+	blockNumber = 4204227; // start of 28
+	blockNumber = 4205479; 
 
-	const endingBlockNumber = 8591976;
-	const data = {};
-	const rows = [];
+	const endingBlockNumber = 5661442;
+	const extrinsics = {
+		vote: [],
+		remove_voter: [],
+	};
 	const stream = csv.format();
 	// stream.pipe(process.stdout);
 
@@ -28,29 +33,40 @@ async function main() {
 		let blockHash = await api.rpc.chain.getBlockHash(blockNumber);
 		console.log(`- block number: ${blockNumber}`);
 		let signedBlock = await api.rpc.chain.getBlock(blockHash);
-		if ((await api.rpc.state.getRuntimeVersion()).toString() > 31) {
-			break;
-		}
-		signedBlock.block.extrinsics.forEach(async (extrinsic) => {
-			if (extrinsic.callIndex[0] == 17) {
-				// Get sender address
-				const sender = extrinsic.signer.toString();
-				data.sender = sender;
-				data.block = blockNumber;
-				// Returns a decorated API instance at the current block hash
-				const apiAt = await api.at(blockHash);
-				const voting = await apiAt.query.electionsPhragmen.voting(sender);
-				data.votingReserve = voting.deposit.toNumber() / 10000000000;
-				const account = await apiAt.query.system.account(sender);
-				data.accountReserves = account.data.reserved.toNumber() / 10000000000;
-				rows.push(data);
-				console.log(rows);
 
-				if (extrinsic.callIndex[1] == 0) {
-					console.log("vote called");
-					// if(data.votingReserve == )
-				} else if (extrinsic.callIndex[1] == 1) {
-					console.log("remove_voter called");
+		signedBlock.block.extrinsics.forEach(async (extrinsic) => {
+			if (extrinsic.callIndex[0] == 17) { // if extrinsic is in the phragmen election pallet
+				if (extrinsic.callIndex[1] == 0 || extrinsic.callIndex[1] == 1) { // `vote` or `remove_voter` extrinsics
+					const sender = extrinsic.signer.toString();
+
+					const currentBlockAPI = await api.at(blockHash);
+					const prevBlockAPI = await api.rpc.chain.getBlockHash(blockNumber - 1).then(hash => api.at(hash));
+					const nextBlockAPI = await api.rpc.chain.getBlockHash(blockNumber + 1).then(hash => api.at(hash));
+					
+
+					const prevReservedAmountInPallet = await prevBlockAPI.query.electionsPhragmen.voting(sender).then(senderVotingValue => senderVotingValue.deposit.toNumber() / PLANCK_PER_DOT);
+
+					const prevReservedAmountFromRelay = await prevBlockAPI.query.system.account(sender).then(account => account.data.reserved.toNumber() / PLANCK_PER_DOT);
+
+					const currentReservedAmountInPallet = await currentBlockAPI.query.electionsPhragmen.voting(sender).then(senderVotingValue => senderVotingValue.deposit.toNumber() / PLANCK_PER_DOT);
+
+					const currentReservedAmountFromRelay = await currentBlockAPI.query.system.account(sender).then(account => account.data.reserved.toNumber() / PLANCK_PER_DOT);
+					
+					const nextReservedAmountInPallet = await nextBlockAPI.query.electionsPhragmen.voting(sender).then(senderVotingValue => senderVotingValue.deposit.toNumber() / PLANCK_PER_DOT);
+
+					const nextReservedAmountFromRelay = await nextBlockAPI.query.system.account(sender).then(account => account.data.reserved.toNumber() / PLANCK_PER_DOT);
+
+					// if (reservedAmountFromRelay == 0.05) {
+
+					// }
+
+					
+
+					// extrinsics.push({
+					// 		sender,
+					// 		blockNumber,
+					// });
+					console.log();
 				}
 			}
 		});
